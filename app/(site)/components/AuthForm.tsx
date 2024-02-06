55a630bddb8350e134012d74b3ad2a8290c110b6
lawtlee <lawsac2947@gmail.com>
2024-02-05 18:05:26 -0800

    The code for week 2
    This is the planned coding portion of Week 2. We will dive a little into
    useState variables, how to utilize state functions and how to just code
    using react and typescript

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import  { useCallback, useEffect, useState } from "react";
+
+type Variant = "LOGIN" | "REGISTER"
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>("LOGIN");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUser] = useState("");
+
+    const toggleVariant = useCallback(()=>{
+        if (variant == "LOGIN")
+            setVariant("REGISTER")
+        else
+            setVariant("LOGIN")
+    },[variant])
+
+    return (
+        <div
+            className="flex flex-col md:w-[450px] sm:w-[80vw] border-2 border-[#0A2A66]
+            justify-center items-center text-left text-[14px] bg-[#0A2A66] text-white
+            gap-2 py-10 rounded-[38px] relative
+        "
+        >
+            <div>
+                <img src="logo.png"/>
+                <p className="text-[20px] font-semibold">Midnights</p>
+            </div>
+            {variant === "REGISTER" && (
+                <div className="flex flex-col items-center w-full gap-1">
+                    <label className="w-[80%]">Username</label>
+                    <input
+                        type="email"
+                        className="placeholder:text-black mb-5 pl-1 text-black rounded-[8px]
+                                    focus:outline-[#F0C420] w-[80%] text-[16px] h-10 bg-[#F6E5A7]"
+                        value={username}
+                        onChange={(e) => setUser(e.target.value)}
+                    />
+                </div>
+            )}
+
+            <div className="flex flex-col items-center w-full gap-1">
+                <label className="w-[80%]">Email{variant === 'LOGIN' ? "/Username" : ""}</label>
+                <input
+                    type="email"
+                    className="placeholder:text-black mb-5 pl-1 text-black rounded-[8px]
+                                focus:outline-[#F0C420] w-[80%] text-[16px] h-10 bg-[#F6E5A7]"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col items-center w-full gap-1">
+                <label className="w-[80%]">Password</label>
+                <input
+                    type="password"
+                    className="placeholder:text-black mb-5 pl-1 text-black rounded-[8px]
+                focus:outline-[#F0C420] w-[80%] text-[16px] h-10 bg-[#F6E5A7]"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="h-10 w-[80%] border-[#252062] bg-[#D4A9FF] 
+                border-[1px] rounded-[8px] text-black flex items-center justify-center mt-2 cursor-pointer select-none
+                "
+                onClick={()=>{
+
+                }}
+            >
+                {variant === 'LOGIN' ? "Sign In" : "Sign Up"}
+            </div>
+            <div className="flex gap-2 justify-center mt-6 px-2">
+                <div>
+                    {variant === 'LOGIN' ? "Don't have an account?" : "Already have an account?"}
+                </div>
+                <div className="font-bold cursor-pointer select-none"
+                    onClick={toggleVariant}
+                >
+                    {variant === 'LOGIN' ? "Sign Up" : "Sign In"}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AuthForm
---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="min-h-full flex flex-col justify-center items-center">
+        <AuthForm/>
+    </div>
+  );
+}
